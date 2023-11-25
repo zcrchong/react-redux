65650fad2667ba3f03f93267ffcf5259ddef06eb
chaoranzuo <15269876632@163.com>
2023-11-25 11:19:54 +0800

    feat: Provider功能解析

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -6,32 +6,41 @@ import { createSubscription } from '../utils/Subscription'
 import { useIsomorphicLayoutEffect } from '../utils/useIsomorphicLayoutEffect'
 import type { Action, AnyAction, Store } from 'redux'
 import type { CheckFrequency } from '../hooks/useSelector'
+/**
+ * 第一部分： Provider注入Store
+ * Provider 做了哪些事。
+1 首先知道 React-Redux 是通过 context 上下文来保存传递 Store 的，但是上下文 value 保存的除了 Store 还有 subscription 。
+2 subscription 可以理解为订阅器，在 React-redux 中一方面用来订阅来自 state 变化，另一方面通知对应的组件更新。在 Provider 中的订阅器 subscription 为根订阅器，
+3 在 Provider 的 useEffect 中，进行真正的绑定订阅功能，其原理内部调用了 store.subscribe ，只有根订阅器才会触发store.subscribe，至于为什么，马上就会讲到。
 
+ * */
 export interface ProviderProps<A extends Action = AnyAction, S = unknown> {
   /**
-   * The single Redux store in your application.
+   * 应用中的单个 Redux 存储。
+   * 这里的 <S, A> 是 TypeScript 中的泛型，分别代表 Redux 的状态（State）和动作（Action）。
    */
-  store: Store<S, A>
+  store: Store<S, A> // 定义一个类型为Store<S, A>的变量store
 
   /**
-   * An optional server state snapshot. Will be used during initial hydration render if available, to ensure that the UI output is consistent with the HTML generated on the server.
+   * 可选的服务器状态快照。在初始 hydration 渲染期间，如果可用，将用于确保 UI 输出与在服务器上生成的 HTML 一致。
    */
-  serverState?: S
+  serverState?: S // 定义一个类型为S的可选变量serverState
 
   /**
-   * Optional context to be used internally in react-redux. Use React.createContext() to create a context to be used.
-   * If this is used, you'll need to customize `connect` by supplying the same context provided to the Provider.
-   * Initial value doesn't matter, as it is overwritten with the internal state of Provider.
+   * 可选的上下文，用于在 react-redux 内部使用。使用 React.createContext() 创建要使用的上下文。
+   * 如果使用此选项，您需要通过向 Provider 提供的相同上下文来自定义 `connect`。
+   * 初始值不重要，因为它被重写为 Provider 的内部状态。
    */
-  context?: Context<ReactReduxContextValue<S, A>>
+  context?: Context<ReactReduxContextValue<S, A>> // 定义一个类型为Context<ReactReduxContextValue<S, A>>的可选变量context
 
-  /** Global configuration for the `useSelector` stability check */
-  stabilityCheck?: CheckFrequency
+  /** `useSelector`稳定性检查的全局配置 */
+  stabilityCheck?: CheckFrequency // 定义一个类型为CheckFrequency的可选变量stabilityCheck，用于`useSelector`稳定性检查的全局配置
 
-  /** Global configuration for the `useSelector` no-op check */
-  noopCheck?: CheckFrequency
+  /** `useSelector` no-op 检查的全局配置 */
+  noopCheck?: CheckFrequency // 定义一个类型为CheckFrequency的可选变量noopCheck，用于`useSelector` no-op 检查的全局配置
 
-  children: ReactNode
+  /** 子组件 */
+  children: ReactNode // 定义一个类型为ReactNode的变量children，代表子组件
 }
 
 function Provider<A extends Action = AnyAction, S = unknown>({
@@ -42,8 +51,11 @@ function Provider<A extends Action = AnyAction, S = unknown>({
   stabilityCheck = 'once',
   noopCheck = 'once',
 }: ProviderProps<A, S>) {
+  /* 利用useMemo，跟据store的变化创建出一个contextValue 包含一个根元素订阅器和当前store  */
   const contextValue = React.useMemo(() => {
+    /* 创建了一个根级 Subscription 订阅器 */
     const subscription = createSubscription(store)
+    /* 根据store 改变创建新的contextValue */
     return {
       store,
       subscription,
@@ -58,20 +70,23 @@ function Provider<A extends Action = AnyAction, S = unknown>({
   useIsomorphicLayoutEffect(() => {
     const { subscription } = contextValue
     subscription.onStateChange = subscription.notifyNestedSubs
-    subscription.trySubscribe()
+    /* 触发trySubscribe方法执行，创建listens */
+    subscription.trySubscribe() // 发起订阅
 
     if (previousState !== store.getState()) {
       subscription.notifyNestedSubs()
     }
     return () => {
+      // 卸载订阅
       subscription.tryUnsubscribe()
       subscription.onStateChange = undefined
     }
-  }, [contextValue, previousState])
+  }, [contextValue, previousState]) // /*  contextValue state 改变出发新的 effect */
 
   const Context = context || ReactReduxContext
 
   // @ts-ignore 'AnyAction' is assignable to the constraint of type 'A', but 'A' could be instantiated with a different subtype
+  // 有一种类型 'AnyAction' 可以赋值给类型 'A'，但 'A' 可能会用不同的子类型进行实例化。
   return <Context.Provider value={contextValue}>{children}</Context.Provider>
 }
 
